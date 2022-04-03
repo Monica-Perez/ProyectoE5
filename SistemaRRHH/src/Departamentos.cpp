2ea0386cd9367c5cb22820ad36e4d613c477118a
Daniel Hall <dhalla@miumg.edu.gt>
2022-04-03 17:16:42 -0600

    Correccion Departamentos

---
+++ SistemaRRHH/src/Departamentos.cpp
@@ -0,0 +1,16 @@
+#include "Departamentos.h"
+#include <iostream>
+
+using namespace std;
+Departamentos::Departamentos()
+{
+    //ctor
+    cout<<"\t\t\t\t------------------"<<endl;
+	cout<<"\t\t\t\t|  DEPARTAMENTOS  |"<<endl;
+	cout<<"\t\t\t\t------------------"<<endl;
+}
+
+Departamentos::~Departamentos()
+{
+    //dtor
+}
