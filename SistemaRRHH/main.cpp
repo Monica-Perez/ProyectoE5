8dfbfa9d1fb3dec7b733c7ca151deeb9cbc58f6d
Monica-Perez <mperezv32@miumg.edu.gt>
2022-04-07 20:58:03 -0600

    Subiendo Mantenimiento de Empleados y Empresas

--- SistemaRRHH/main.cpp
+++ SistemaRRHH/main.cpp
@@ -3,17 +3,13 @@
 #include "Puestos.h"
 #include "Departamento.h"
 #include "Impuestos.h"
+#include "Empresas.h"
 #include "Nomina.h"
-#include <fstream>
-#include <iomanip>
-#include <cstdlib>
 
 using namespace std;
 
-void crearArchivoPuestos();
 int escribirOpcion();
-
-enum Opciones { empleados = 1, puestos, departamentos, impuestos, nomina, FIN };
+enum Opciones { empleados = 1, puestos, departamentos, impuestos, empresas, nomina, FIN };
 
 int main()
 {
@@ -21,55 +17,57 @@ int main()
     while (( opcion = escribirOpcion() ) != FIN)
     {
         switch ( opcion ) {
-
-         // crear el archivo de texto a partir del archivo de registros
-         case empleados:
-            {
-                Empleado claseempleado;
-            }
+            case empleados:
+                {
+                    Empleado claseempleado;
+                }
             break;
-         case puestos:
-            {
-                Puestos clasepuestos;
-            }
+            case puestos:
+                {
+                    Puestos clasepuestos;
+                }
             break;
-          case departamentos:
-            {
-                Departamento clasedepartamentos;
-            }
+            case departamentos:
+                {
+                    Departamento clasedepartamentos;
+                }
             break;
-          case impuestos:
-            {
-                Impuestos claseimpuestos;
-            }
+            case impuestos:
+                {
+                    Impuestos claseimpuestos;
+                }
             break;
-          case nomina:
-            {
-                Nomina clasenomina;
-            }
+            case empresas:
+                {
+                    Empresas claseempresa;
+                }
+            break;
+            case nomina:
+                {
+                    Nomina clasenomina;
+                }
             break;
         }
     }
-
     return 0;
 }
 
-
 int escribirOpcion(){
-   // mostrar opciones disponibles
-   cout<<"\t\t\t---------------------------------"<<endl;
+    system("cls");
+    cout<<"\t\t\t---------------------------------"<<endl;
 	cout<<"\t\t\t |   SISTEMA RECURSOS HUMANOS  |"<<endl;
 	cout<<"\t\t\t---------------------------------"<<endl;
 
     cout << "\nEscriba su opcion" << endl
-        << "1 - Empleados" << endl
-        << "2 - Puestos" << endl
-        << "3 - Departamentos" << endl
-        << "4 - Impuestos" << endl
-        << "5 - Generacion de Nomina" << endl
-        << "6 - Finalizar el Programa\n R - ";
+        << " 1. Empleados" << endl
+        << " 2. Puestos" << endl
+        << " 3. Departamentos" << endl
+        << " 4. Impuestos" << endl
+        << " 5. Empresas" << endl
+        << " 6. Generacion de Nomina" << endl
+        << " 7. Finalizar el Programa\n  R - ";
    int opcionMenu;
-   cin >> opcionMenu; // recibir opción del usuario
+   cin >> opcionMenu;
 
    return opcionMenu;
 
